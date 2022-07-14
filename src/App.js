229010c76c27ac42748bcc6312c5c4655e48b7ff
Gadzhi <gadzhievs69@gmail.com>
2022-07-15 02:30:35 +0300

    Hook-state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React from 'react';
-import './App.css';
-import Card from './components.js/Card';
-import CardImg from './components.js/CardImg';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="container" >
-      <Card
-        width="18rem"
-        title="Card title"
-        text="Some quick example text to build on the card title and make up the bulk of the card's content."
-        btnName="Go somewhere"
-      >
-        <CardImg 
-          src="https://picsum.photos/300/200"
-          alt="Plug"
-        />
-      </Card>
+    <div>
+      
     </div>
   );
 }
---
+++ src/Hooks/state.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+function computeInitialCounter() {
+  console.log('Some calculations...')
+  return Math.trunc(Math.random() * 20)
+  //Вычислительная функция
+}
+
+function App() {
+  // const [counter, setCounter]  = useState(0) //Создание состояния
+  const [counter, setCounter]  = useState(() => {
+    return computeInitialCounter()
+    //Оптимизация состояния.
+  })
+
+  const [state, setState] = useState({
+    title: 'Счетчик',
+    date: Date.now()
+  })
+
+  function increment() {
+    // setCounter(counter + 1) 
+    // setCounter(counter + 1)
+    
+    setCounter((prevCounter) => {
+      return prevCounter + 1
+      //Создание collback функции
+    })
+
+    // setCounter(prev => prev + 1) //Увеличиваем на 2
+
+  }
+
+  function decrement() {
+    setCounter(counter - 1)
+  }
+
+  function updateTitle() {
+    setState(prev => {
+      return {
+        ...prev,
+        title: 'Новое название'
+      }
+    })
+    //Функция которая меняет состояние, сохраняя предыдущее...
+  }
+
+  return (
+    <div>
+      <h1>Счетчик: {counter}</h1>
+      <button onClick={increment} className='btn btn-success'>Добавить</button>
+      <button onClick={decrement} className='btn btn-danger'>Убрать</button>
+      <button onClick={updateTitle} className='btn btn-default'>Изменить название</button>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre> {/* Вывод состояния в браузер */}
+    </div>
+  );
+}
+
+export default App;
