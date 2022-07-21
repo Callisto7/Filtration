d2323f108d27e2bb87d22e84242b681c08726a01
Gadzhi <gadzhievs69@gmail.com>
2022-07-21 15:05:22 +0300

    Created an application that formats the publication date

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
-import { nanoid } from 'nanoid';
-import { useState } from 'react';
-import './index.css';
-import Clock from './components/Clock/Clock';
-import Form from './components/Form/Form';
+import React, { useState } from 'react';
+import Videos from './data/Videos';
+import VideoList from './components/VideoList';
 
 function App() {
-  const [clocks, setClocks] = useState([]);
-
-  function handleFormSubmit(form) {
-    setClocks((prevState) => [...prevState, {
-      id: nanoid(),
-      name: form.name,
-      userTimezone: form.userTimezone,
-    }]);
-  }
-
-  function getClockIndex(id) {
-    const index = clocks.findIndex((clock) => clock.id === id);
-
-    return index;
-  }
-
-  function handleDeleteClick(id) {
-    const index = getClockIndex(id);
-
-    const updatedClocks = [
-      ...clocks.slice(0, index),
-      ...clocks.slice(index + 1),
-    ];
-
-    setClocks(updatedClocks);
-  }
+  const [list] = useState(Videos);
 
   return (
-    <div className="App-container">
-      <Form onFormSubmit={handleFormSubmit} />
-      <div className="App-clocks-container">
-        {clocks.map((clock) => {
-          return (
-            <Clock
-              key={clock.id}
-              id={clock.id}
-              name={clock.name}
-              userTimezone={clock.userTimezone}
-              onDeleteClick={handleDeleteClick}
-            />
-          );
-        })}
-      </div>
-    </div>
+      <VideoList list={list} />
   );
 }
 
