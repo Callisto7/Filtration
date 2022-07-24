34dfda9931581700b544b545ea31214054d18f2a
Gadzhi <gadzhievs69@gmail.com>
2022-07-24 20:00:09 +0300

    Complete homework

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import './index.css'
-import List from "./components/List";
-import Details from "./components/Details";
-import useFetch from "./components/useFetch";
 
+import First from './components/First';
+import Second from './components/Second';
+import Third from './components/Third';
 
 function App() {
-  const [listItems, setListItems] = useState([]);
-  const [info, setInfo] = useState({ id: '', name: '' })
-  const { get, loading } = useFetch('https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/');
-  
-
-  
-  useEffect(() => {
-    get('users.json')
-      .then((data) => setListItems(data))
-      .catch((error) => console.error(error));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-    function handleClick ({target}) {
-      const id = target.closest('.list_item').id;
-
-      setInfo({ id: id, name: target.textContent});
-    }
-
   return (
     <>
-      <List onItemClick={handleClick} loading={loading}>
-        {listItems}
-      </List>
-      {info.id !== '' && <Details {...info} get={get}/>}
+      <First />
+      <Second />
+      <Third />
     </>
-  )
-
+  );
 }
 
-export default App
+export default App;
